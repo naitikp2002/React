d3d290b1d1277f6f086ae0afec58e8ef9462d02f
Naitik Patel <naitik2002patel@gmail.com>
2024-02-19 17:50:57 +0530

    add: multiple child inside div using [ ]

--- demo/App.js
+++ demo/App.js
@@ -1,3 +1,8 @@
-const heading = React.createElement("h1", {}, "Hello world");
+const parent = React.createElement("div", {}, [
+  React.createElement("h1", {}, "Hello world!"),
+  React.createElement("h1", {}, "Hello universe!"),
+]);
+
+// const heading = React.createElement("h1", {}, "Hello world");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
