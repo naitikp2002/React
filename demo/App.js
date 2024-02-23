6c46f137015986b4cd8c16e8cbff2215c6b714bc
Naitik Patel <naitik2002patel@gmail.com>
2024-02-23 18:17:04 +0530

    add: jsx into app.js

--- demo/App.js
+++ demo/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement("div", {}, [
-  React.createElement("h1", {}, "Hello world!"),
-  React.createElement("h1", {}, "Hello universe!"),
-]);
+const Parent = () => {
+  return <h1>Hello Welcome To React App</h1>;
+};
 
+const HeadingComponent = () => {
+  return (
+    <>
+      <div>
+        <Parent />
+        <h1>This is Heading </h1>
+      </div>
+    </>
+  );
+};
 // const heading = React.createElement("h1", {}, "Hello world");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
